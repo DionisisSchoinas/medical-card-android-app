29f8e8065835bf54068cc6895c93038ff7a50919
Schoinas Dionisis <schinasdionisis@gmail.com>
2021-05-12 18:15:35 +0300

    Switched to live-server
    https://medical-card-api.herokuapp.com/

--- app/src/main/java/com/unipi/p17134/medicalcard/API/BaseDAO.java
+++ app/src/main/java/com/unipi/p17134/medicalcard/API/BaseDAO.java
@@ -9,7 +9,8 @@ import com.unipi.p17134.medicalcard.R;
 import org.json.JSONObject;
 
 public class BaseDAO {
-    protected static final String url = "http://192.168.1.4:3000";
+    //protected static final String url = "http://192.168.1.4:3000";
+    protected static final String url = "https://medical-card-api.herokuapp.com";
     public static final String APPOINTMENT_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
     public static final String USER_DATE_OF_BIRTH_FORMAT = "yyyy-MM-dd";
 }
